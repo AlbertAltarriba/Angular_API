2c37d643ac2fbcfbd14fa17001d7aa307509797d
Albert Altarriba <albertaltfer@gmail.com>
2022-08-02 18:12:56 +0200

    Init
    Creación proyecto, components, service, interface y bootstrap

---
+++ Angular_API/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FormComponent } from './components/form/form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserCardComponent } from './components/user-card/user-card.component';
+import { UserViewComponent } from './components/user-view/user-view.component';
+import { ErrorPageComponent } from './components/error-page/error-page.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    UserListComponent,
+    UserCardComponent,
+    UserViewComponent,
+    ErrorPageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
